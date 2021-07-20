844b600fc13c4d0bdb8dddcdb4dde6ead72f1302
yanwei <yanwei@douyu.tv>
2021-07-20 19:56:27 +0800

    test

--- 02_package/main/main.go
+++ 02_package/main/main.go
@@ -2,8 +2,8 @@ package main
 
 import (
 	"fmt"
+
 	"github.com/GoesToEleven/GolangTraining/02_package/stringutil"
-	"github.com/GoesToEleven/GolangTraining/02_package/icomefromalaska"
 	//someAlias "github.com/GoesToEleven/GolangTraining/02_package/icomefromalaska"
 )
 
@@ -11,4 +11,5 @@ func main() {
 	fmt.Println(stringutil.Reverse("!oG ,olleH"))
 	fmt.Println(stringutil.MyName)
 	fmt.Println(winniepooh.BearName)
+	fmt.Println("hello world!")
 }
